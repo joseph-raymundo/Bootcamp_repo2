30bcb0df42a34fe71c0b2eff2dbe99392aae8ef0
Joseph Raymundo <joseph.raymundo@accenture.com>
2025-06-05 21:11:37 +0000

    Changes Complete

--- controller/MainView.controller.js
+++ controller/MainView.controller.js
@@ -5,14 +5,16 @@ sap.ui.define([
 /**
  * @param {typeof sap.ui.core.mvc.Controller} Controller
  */
-    (Controller, MessageToast) => {
-    "use strict";
+   function (Controller, MessageToast) {
+    //"use strict";
 
     return Controller.extend("com.training.exer1josephraymundo.controller.MainView", {
         onInit() {
         },
         onAddItem: function (){
-            this.fnDisplayMsg("Add button pressed");
+            var oTextBundle = this.fnDisplayMsg("Add button pressed");
+            var sMsg = oTextBundle.getText("addButtonMsg");
+                this.fnDisplayMsg(sMsg);
         },
 
         fnDisplayMsg: function (sMsg){
@@ -21,19 +23,41 @@ sap.ui.define([
         onChangeMOP: function (oEvent) {
             var sSelectedKey = oEvent.getParameter("selectedItem").getProperty("key");
             var oMobileLabel = this.getView().byId("idLblPhone");
-            var oMobileInput = this.getView().byId("idInputPhone");
+            var oMobileInput = this.getView().byId("idInputPhone");            
+            var oCcNumLabel = this.getView().byId("idLblCcNum");
+            var oCcNumInput = this.getView().byId("idInputCcNum");
+            var sSelectedText = oEvent.getParameter("selectedItem").getText();
 
             if (sSelectedKey === "GCASH"){
-                // show the mobile field
+                // show the mobile field, hide credit card field
                 oMobileLabel.setVisible(true);
                 oMobileInput.setVisible(true);
-            } else {
+                oCcNumLabel.setVisible(false);
+                oCcNumInput.setVisible(false);                
+            } else if (sSelectedKey === "CC"){
+                //Show credit card field, hide mobile field
                 oMobileLabel.setVisible(false);
                 oMobileInput.setVisible(false);
+                oCcNumLabel.setVisible(true);
+                oCcNumInput.setVisible(true);
+            } else {
+                //Hide all optional fields
+                oMobileLabel.setVisible(false);
+                oMobileInput.setVisible(false);
+                oCcNumLabel.setVisible(false);
+                oCcNumInput.setVisible(false);
+            } 
+            this.fnDisplayMsg(sSelectedText);
+        },
+        onPressCheckout: function (){
+            var oInputFNameValue = this.getView().byId("idInptFName").getValue();
+            var oInputLNameValue = this.getView().byId("idInptLName").getValue();
+
+            // Check if first name is blank
+            if (oInputFNameValue === "" || oInputLNameValue === ""){
+                sap.m.MessageToast.show("Required Field is blank"); 
             }
         }
-
-
-    });
+    } );
     
 });
